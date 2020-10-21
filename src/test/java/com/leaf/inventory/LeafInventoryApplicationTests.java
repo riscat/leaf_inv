8c0ca9b53dd057f13e8e0841f420f4352e158f73
Ralp Jules <raprap.sicat@gmail.com>
2020-10-21 18:53:25 +0800

    Initial Commit

---
+++ src/test/java/com/leaf/inventory/LeafInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaf.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
